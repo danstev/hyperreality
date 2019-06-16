fe31a73218973111349949aa2a591f7669cbeca0
Dan Ward <dan.steve@hotmail.co.uk>
2019-06-16 18:27:14 +0100

    door and switch scripts

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    bool state = false;
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.Play("Close", 0, 0.25f);
+    }
+
+    void Interact()
+    {
+        if(state == false)
+        {
+            anim.Play("Door", 0, 0.25f);
+            state = true;
+        }
+        else
+        {
+            anim.Play("Close", 0, 0.25f);
+            state = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour 
+{
+
+    void Interact()
+    {
+        foreach (Transform child in transform)
+        {
+            if(child.gameObject.activeSelf == true)
+            {
+                child.gameObject.SetActive(false);
+            }
+            else
+            {
+                child.gameObject.SetActive(true);
+            }
+            
+        }
+    }
+}
